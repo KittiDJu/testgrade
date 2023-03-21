60b03de4544263cc9321db7fc45123f373d72781
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-21 23:03:28 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "World3212"}
+    assert response.json() == {"Hello": "World"}
 
 def test_get_grade():
     response = client.get("/grade/85")
