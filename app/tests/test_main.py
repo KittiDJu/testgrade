492c4ec35932ceb59de5ad270937d4ba36797fdc
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-21 23:00:12 +0700

    Update test_main.py

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,1 +1,25 @@
+from fastapi.testclient import TestClient
+from app.main import app
 
+client = TestClient(app)
+
+def test_get_grade():
+    response = client.get("/grade/85")
+    assert response.status_code == 200
+    assert response.json() == {"grade": "A"}
+
+    response = client.get("/grade/75")
+    assert response.status_code == 200
+    assert response.json() == {"grade": "B"}
+
+    response = client.get("/grade/65")
+    assert response.status_code == 200
+    assert response.json() == {"grade": "C"}
+
+    response = client.get("/grade/55")
+    assert response.status_code == 200
+    assert response.json() == {"grade": "D"}
+
+    response = client.get("/grade/45")
+    assert response.status_code == 200
+    assert response.json() == {"grade": "F"}
