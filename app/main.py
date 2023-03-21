98c6e5a9b1b52d75d6a2da77c15b15d38e11fac2
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-21 22:58:07 +0700

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/grade/{score}")
+def get_grade(score: int):
+    if score >= 80:
+        grade = "A"
+    elif score >= 70:
+        grade = "B"
+    elif score >= 60:
+        grade = "C"
+    elif score >= 50:
+        grade = "D"
+    else:
+        grade = "F"
+    return {"grade": grade}
+
+handler = Mangum(app)
